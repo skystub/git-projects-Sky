9af3707d5ac3d7ad7a13c98ceba24830695c7f01
sky <sstubbeman1@hwemail.com>
2024-09-24 23:13:16 -0700

    Tried to implement a zip-compression ability into createUniqueFileName
    Controlled by a global variable compressionEnabled

--- Blob.java
+++ Blob.java
@@ -10,12 +10,13 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.BufferedOutputStream;
 import java.util.zip.Deflater;
+import java.io.ByteArrayOutputStream;
 
 
 public class Blob {
     public static boolean compressionEnabled;
     public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException, IOException {
-        compressionEnabled = false;
+        compressionEnabled = true;
         createNewBlob("/Users/skystubbeman/Desktop/tester.txt","/Users/skystubbeman/Documents/HTCS_Projects/git");
     }
 
@@ -27,25 +28,39 @@ public class Blob {
         BufferedInputStream br = new BufferedInputStream(in);
         MessageDigest sha1Digest = MessageDigest.getInstance("SHA-1");
 
-        byte[] byteArr = new byte[8192];
-        int length = br.read(byteArr);
+        byte[] inputBytes = br.readAllBytes();
+        byte[] endingBytes;
 
-        while (length != -1) {
-            sha1Digest.update(byteArr, 0, length);
-            length = br.read(byteArr);
+        in.close();
+        br.close();
+
+        if(compressionEnabled){
+            Deflater deflater = new Deflater();
+            deflater.setInput(inputBytes);
+            deflater.finish();
+
+            byte[] output = new byte[inputBytes.length * 2];
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            while (!deflater.finished()){
+                int num = deflater.deflate(output);
+                outputStream.write(output, 0, num);
+            }
+            endingBytes = outputStream.toByteArray();
+            outputStream.close();
+        }
+        else{
+           endingBytes = inputBytes;
         }
 
+        sha1Digest.update(endingBytes);
         byte[] hash = sha1Digest.digest();
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < hash.length; i++) {
             sb.append(String.format("%02x", hash[i]));
         }
-        in.close();
-        br.close();
 
         return sb.toString();
-
     }
 
     public static void createNewBlob(String filePath, String gitRepoPath) throws FileNotFoundException, NoSuchAlgorithmException, IOException{
@@ -75,7 +90,4 @@ public class Blob {
         bw.close();
     }
 
-    public static void compressBlob(){
-       
-    }
 }
